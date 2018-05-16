78bb2e29f09f50323d9b2c4b861a4cc6a509846f
CraigMacIvor <Craig MacIvor>
2018-05-16 13:12:59 -0400

    add unit tests

---
+++ PetStoreTests/OrderTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using BusinessTier;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PetStoreTests
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        public void CalculateTotalCost_WhenCalled_SumsOrderItemPrices()
+        {
+
+            //Arrange
+            var orderItems = new List<OrderDetail>();
+            orderItems.Add(new OrderDetail
+            {
+                 ProductId = "70508895",
+                 ProductName = "Scratching Post",
+                 ProductPrice = 32.95m
+            });
+            orderItems.Add(new OrderDetail
+            {
+                ProductId = "8ed0e6f7",
+                ProductName = "Stroller",
+                ProductPrice = 124.95m
+            });
+
+            var order = new BusinessTier.Order
+            {
+                CustomerId = "123",
+                OrderItems = orderItems
+            };
+
+            //Act
+            order.CalculateTotalCost();
+
+
+            //Assert
+            Assert.AreEqual(157.90m, order.TotalCost);
+        }
+    }
+}
---
+++ PetStoreTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PetStoreTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PetStoreTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6b0de0b4-4493-4c89-bd35-77991a70e426")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
